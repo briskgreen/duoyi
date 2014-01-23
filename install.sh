35952241000f9194f36773d306e7acd007cc579a
brisk <briskgreen@163.com>
2014-01-24 02:46:21 +0800

    更新

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ mkdir -p /usr/local/duoyi/
 cp -r img /usr/local/duoyi/
 cp duoyi preferences /usr/local/duoyi/
 cp .duoyirc /usr/local/duoyi/
+cp readme /usr/local/duoyi/
+cp LICENSE /usr/local/duoyi/
 
 ln -s /usr/local/duoyi/duoyi /bin/duoyi
 
